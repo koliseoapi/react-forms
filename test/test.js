99e5d7cc8c0de7213ff839cbdcf5476c05c34024
Nacho Coloma <icoloma@gmail.com>
2017-01-09 20:42:19 +0100

    Form was invoking onSubmit even if validation was not passing

--- test/test.js
+++ test/test.js
@@ -90,12 +90,25 @@ describe('Input', function() {
 
 describe('Form', function() {
 
+  function submit({ callback, state }) {
+    const form = mount(<Form onSubmit={callback}><Input type="text" required state={state} name="name" /></Form>);
+    return form.instance().onSubmit({ preventDefault: noop });
+  }
+
   it('should call onSubmit only if validation passes', function() {
     const state = { };
     const callback = sinon.spy();
-    const wrapper = mount(<Form onSubmit={callback}><Input type="text" required state={state} name="name" /></Form>);
-    const input = wrapper.find('form').simulate('submit');
-    assert(!callback.called, 'Invoked onSubmit() when validation was not passing');
+    return submit({ callback, state }).then(() => {
+      assert(!callback.called, 'Invoked onSubmit() when validation was not passing');
+    });
+  });
+
+  it('should call onSubmit if validation passes', function() {
+    const state = { name: 'foo' };
+    const callback = sinon.spy();
+    return submit({ callback, state }).then(() => {
+      assert(callback.called, 'Not invoked onSubmit() when validation was passing');
+    });
   });
 
 });
