828a109fe441ab20e7435e30971045c613ed7a9a
Nacho Coloma <icoloma@gmail.com>
2020-06-29 23:17:56 +0200

    Export Context object from index

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { ValidationActions } from "./core/ValidationActions";
 import { Input, TextArea, Select } from "./components/InputElements";
 import { Form, FieldSet } from "./components/Form";
 import { Button } from "./components/Button";
+import { I18nContext } from "./components/I18nContext";
 
 export {
+  I18nContext,
   Form,
   FieldSet,
   Button,
