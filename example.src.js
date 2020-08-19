2fb50a23d5083005830bfaca7d20b2518292557b
Nacho Coloma <icoloma@gmail.com>
2020-08-19 16:04:09 +0200

    Merge master

--- example.src.js
+++ example.src.js
@@ -5,7 +5,8 @@
 */
 import { Form, Input, Button } from "../src/index";
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FormContext } from "../src/components/Form";
 
 interface MyState {
   name: string;
@@ -25,101 +26,72 @@ window.addEventListener("error", function (err) {
   output.innerHTML = err.message;
 });
 
-const state: MyState = {
+const initialState: MyState = {
   name: "John Doe",
   age: 23,
   subscribed: true,
   gender: "other",
 };
 
-function onSubmit() {
-  console.log("onSubmit() invoked successfully");
-  printState();
-  return Promise.resolve();
-}
-
-function printState() {
-  output.classList.remove("error");
-  output.classList.add("highlight");
-  output.innerHTML = JSON.stringify(state);
-  setTimeout(() => output.classList.remove("highlight"), 0);
-}
-
 function MyApp() {
+  const [output, setOutput] = useState(initialState);
+  const [outputClass, setOutputClass] = useState("");
+
+  function onSubmit(newState: MyState) {
+    console.log(
+      "onSubmit() invoked with: " + JSON.stringify(newState, undefined, "  ")
+    );
+    setOutputClass("highlight");
+    setOutput(newState);
+    setTimeout(() => setOutputClass(""), 0);
+    return Promise.resolve();
+  }
+
   return (
-    <Form onSubmit={onSubmit} initialValues={state}>
-      <label htmlFor="name">Name</label>
-      <Input
-        name="name"
-        type="text"
-        autoComplete="off"
-        required
-        maxLength={50}
-        onChange={printState}
-        aria-describedby="name-desc"
-      />
-      <p id="name-desc">Introduce the first and last name of the user</p>
-      <label htmlFor="age">Age</label>
-      <Input
-        name="age"
-        type="number"
-        min="0"
-        max="120"
-        required
-        onChange={printState}
-      />
-      <label>
-        <Input name="subscribed" type="checkbox" onChange={printState} />{" "}
-        Subscribe to newsletter
-      </label>
-      <label>
+    <>
+      <Form onSubmit={onSubmit} initialValues={initialState}>
+        <label htmlFor="name">Name</label>
         <Input
-          name="gender"
-          type="radio"
-          defaultValue="male"
-          onChange={printState}
-        />{" "}
-        Male
-      </label>
-      <label>
+          name="name"
+          type="text"
+          autoComplete="off"
+          required
+          maxLength={50}
+          aria-describedby="name-desc"
+        />
+        <p id="name-desc">Introduce the first and last name of the user</p>
+        <label htmlFor="age">Age</label>
+        <Input name="age" type="number" min="0" max="120" required />
+        <label>
+          <Input name="subscribed" type="checkbox" /> Subscribe to newsletter
+        </label>
+        <label>
+          <Input name="gender" type="radio" defaultValue="male" /> Male
+        </label>
+        <label>
+          <Input name="gender" type="radio" defaultValue="female" /> Female
+        </label>
+        <label>
+          <Input name="gender" type="radio" defaultValue="other" /> Other
+        </label>
+        <label htmlFor="expires">Expires</label>
         <Input
-          name="gender"
-          type="radio"
-          defaultValue="female"
-          onChange={printState}
-        />{" "}
-        Female
-      </label>
-      <label>
-        <Input
-          name="gender"
-          type="radio"
-          defaultValue="other"
-          onChange={printState}
-        />{" "}
-        Other
-      </label>
-      <label htmlFor="expires">Expires</label>
-      <Input
-        name="expires"
-        type="date"
-        min={new Date().toISOString().substring(0, 10)}
-        required
-        pattern="\d{4}-\d{2}-\d{2}"
-        onChange={printState}
-      />
-      <label htmlFor="time">Time</label>
-      <Input
-        name="time"
-        id="time"
-        type="time"
-        pattern="[0-9]{2}:[0-9]{2}"
-        onChange={printState}
-      />
-      <Button>Submit form and see resulting state</Button>
-    </Form>
+          name="expires"
+          type="date"
+          min={new Date().toISOString().substring(0, 10)}
+          required
+          pattern="\d{4}-\d{2}-\d{2}"
+        />
+        <label htmlFor="time">Time</label>
+        <Input name="time" id="time" type="time" pattern="[0-9]{2}:[0-9]{2}" />
+        <Button>Submit form and see resulting state</Button>
+      </Form>
+      <p>Form state:</p>
+      <pre className={`output ${outputClass}`}>
+        {JSON.stringify(output, undefined, "  ")}
+      </pre>
+    </>
   );
 }
 
 ReactDOM.render(<MyApp />, document.getElementsByClassName("container")[0]);
-printState();
